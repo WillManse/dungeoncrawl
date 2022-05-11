44b6ed855da25cf3340da12bdb95e0735a41fac6
WillMansell <Wombat2000@yahoo.com>
2022-05-11 11:21:16 +0100

    Spawn monsters

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+
 pub fn spawn_player(ecs: &mut World, pos: Point) {
     ecs.push((
         Player,
@@ -6,6 +7,22 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
         Render {
             color: ColorPair::new(WHITE, BLACK),
             glyph: to_cp437('@'),
+        },
+    ));
+}
+
+pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
+    ecs.push((
+        Enemy,
+        pos,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: match rng.range(0, 4) {
+                0 => to_cp437('E'),
+                1 => to_cp437('O'),
+                2 => to_cp437('o'),
+                _ => to_cp437('g'),
+            },
         },
     ));
 }

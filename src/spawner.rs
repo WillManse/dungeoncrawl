15d761f75ca986296e90d449529f0d5b4554ccb2
WillMansell <Wombat2000@yahoo.com>
2022-05-18 09:08:09 +0100

    HUD for player health

--- src/spawner.rs
+++ src/spawner.rs
@@ -6,7 +6,11 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
         pos,
         Render {
             color: ColorPair::new(WHITE, BLACK),
-            glyph: to_cp437('@'),
+            glyph: to_cp437('0'),
+        },
+        Health {
+            current: 20,
+            max: 20,
         },
     ));
 }
@@ -18,10 +22,10 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
         Render {
             color: ColorPair::new(WHITE, BLACK),
             glyph: match rng.range(0, 4) {
-                0 => to_cp437('E'),
-                1 => to_cp437('O'),
-                2 => to_cp437('o'),
-                _ => to_cp437('g'),
+                0 => to_cp437('1'),
+                1 => to_cp437('2'),
+                2 => to_cp437('3'),
+                _ => to_cp437('4'),
             },
         },
         MovingRandomly {},
